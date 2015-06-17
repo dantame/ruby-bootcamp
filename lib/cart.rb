5e917ff80115995df770dde23a114e1fbc0688b7
Tame, Dan <Dan.Tame@sky.uk>
2015-06-17 14:08:46 +0100

    Only calculate total when adding or removing items so that it doesn't have to be re-calculated on every access of the total function

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,22 +1,34 @@
 class Cart
 	attr_reader :items
+	attr_reader :total
 
 	def initialize prices
 		@prices = prices
 		@items = []
+		@total = 0
 	end
 
 	def add_items! *items
 		@items.push *items
+
+		@total = calculate_total
+
+		return @items
 	end
 
 	def remove_items! *items
 		items.each do |item|
 			@items.delete_at @items.index(item)
 		end
+
+		@total = calculate_total
+
+		return @items
 	end
 
-	def total
+	private
+
+	def calculate_total
 		@items.inject(0) do |sum, item|
 			sum += @prices[item] if @prices.key?(item)
 			sum
